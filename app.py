49db7e6d1c2b8c010e00a6f6fe128a04a87c3e73
Adviteeya Shrav <shravadviteeya70@gmail.com>
2022-06-15 20:03:42 +0530

    makes some changes in the app file

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route("/", methods= ['GET','POST'])
 def index():
     return "Starting Machine Learning Projects "
-
+    return " Kya ho raha hai khushbu bhot ladai karte ho "
+    return " Kya ho raha hai swati bhot thak jati ho tum kaam karte karte karte ho "
+    return " Kya ho raha hai jyoti  bhot gym jane lage ho aur wahan ladai  karte ho "
 
 
 if __name__=="__main__":
