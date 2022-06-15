752457d3db2a64a97dd56af75efa1a71dbe1291e
Adviteeya Shrav <shravadviteeya70@gmail.com>
2022-06-15 20:15:57 +0530

    added  some additional line in app.py

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods= ['GET','POST'])
 def index():
-    return "Starting Machine Learning Projects "
-    return " Kya ho raha hai khushbu bhot ladai karte ho "
-    return " Kya ho raha hai swati bhot thak jati ho tum kaam karte karte karte ho "
-    return " Kya ho raha hai jyoti  bhot gym jane lage ho aur wahan ladai  karte ho "
-
+    return "Starting Machine Learning Projects-- Kya ho raha hai khushbu bhot ladai karte ho -- Kya ho raha hai swati bhot thak jati ho tum kaam karte karte karte ho -- Kya ho raha hai jyoti  bhot gym jane lage ho aur wahan ladai  karte ho "
 
 if __name__=="__main__":
     app.run(debug= True)
