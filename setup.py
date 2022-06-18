c58232d11aedb6a93d64e8abb6f31c0e3dc9fd29
Adviteeya Shrav <shravadviteeya70@gmail.com>
2022-06-18 11:21:55 +0530

    creating the housing main folder and setup file

---
+++ setup.py
@@ -0,0 +1,47 @@
+from gc import get_referents
+from unicodedata import name
+from xml.etree.ElementTree import VERSION
+from importlib_metadata import version
+from setuptools import setup
+from typing import List
+
+
+
+
+
+#DECLARING VARIABLES FOR SETUP FUNCTIONS
+
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.1"
+AUTHOR="ADVITEEYA SHRAV"
+DESCRIPTION= "THIS IS THE FIRST FSDS NOV. BATCH ML PROJECTS "
+PACKAGES=["housing"]
+REQUIREMENTS_FILE_NAME= "requirements.txt"
+
+
+def get_requirements_list()->List[str]:
+
+    """
+    Description: This function is going to retrun list of requirements mention in requirements to txt.file
+
+
+    return This function is going to return a list which contain name of libraries 
+
+    """
+    
+    with open(REQUIREMENTS_FILE_NAME) as requirements_file:
+        return requirements_file.readlines()
+
+setup(
+name= PROJECT_NAME,
+version= VERSION,
+author= AUTHOR,
+description= DESCRIPTION,
+packages= PACKAGES,
+install_requirements = get_requirements_list()
+
+
+)
+
+if __name__=="__main__":
+    print(get_requirements_list())
