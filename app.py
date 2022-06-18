8e6613b295bd04abb67c3438c52dc102a620b47d
Adviteeya Shrav <shravadviteeya70@gmail.com>
2022-06-18 15:09:34 +0530

    updated

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-
-from logging import exception
-import sys
 from flask import Flask
-import housing
-from housing.exception import HousingException
+import sys
 from housing.logger import logging
+from housing.exception import HousingException
 app = Flask(__name__)
 
 @app.route("/", methods= ['GET','POST'])
@@ -12,19 +9,11 @@ def index():
     try:
         raise Exception("We are testing custom exception")
     except Exception as e:
-        housing = HousingException(e,sys)
+        raise HousingException(e,sys) from e
         logging.info(housing.error_message)
         logging.info("We are testing logging module")
-        return "Starting Machine Learning Projects-- "
-    
-
-
-
-
-    #logging.info("We are testing logging modules")
-
-    
-
+    return "Starting Machine Learning Projects-- "
+  
 if __name__=="__main__":
     app.run(debug= True)
 
--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -1,27 +1,20 @@
-# directory in which we will log this file
-from datetime import datetime
-from distutils.log import INFO
 import logging
+from datetime import datetime
 import os
-from tkinter import CURRENT
-import os
-from tkinter.tix import Tree
-LOG_DIR = "housing_logs"
 
-# within log folder we will create file name
-# so when we trigger file at particualr time sampt is created
+LOG_DIR="housing_logs"
 
-CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-LOG_FILE_NAME= f"log_{CURRENT_TIME_STAMP}.log"
+CURRENT_TIME_STAMP=  f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
-os.makedirs(LOG_DIR, exist_ok= True)
+LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
+
+
+os.makedirs(LOG_DIR,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
-logging.basicConfig(filename= LOG_FILE_PATH
-filemode= 'w',
-format= '[%(asctime)s] %(levelname)s - %(message)s',
-level= logging.INFO
-
+logging.basicConfig(filename=LOG_FILE_PATH,
+filemode="w",
+format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
+level=logging.INFO
 )
-
