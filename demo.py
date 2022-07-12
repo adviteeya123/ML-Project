5f6e9b1495e186def2f8a4ce58fa6a571efe34b7
Adviteeya Shrav <shravadviteeya70@gmail.com>
2022-07-13 02:04:51 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,23 +1,30 @@
-
 from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logger import logging
 from housing.config.configuration import Configuartion
-
+from housing.component.data_transformation import DataTransformation
+import os
 def main():
     try:
-        #pipeline = Pipeline()
+        config_path = os.path.join("config","config.yaml")
+        pipeline = Pipeline(Configuartion(config_file_path=config_path))
         #pipeline.run_pipeline()
-        #data_ingestion_config = Configuartion().get_data_ingestion_config()
-        #data_validation_config = Configuartion().get_data_validation_config()
-        # data_transformation_config = Configuartion().get_data_transformation_config()
-        data_model_training_config = Configuartion().get_model_trainer_config()
-        #data_model_pusher_config = Configuartion().get_model_pusher_config()
-        #data_model_evaluator_config = Configuartion().get_model_evaluation_config()
+        pipeline.start()
+        logging.info("main function execution completed.")
+        # # data_validation_config = Configuartion().get_data_transformation_config()
+        # # print(data_validation_config)
+        # schema_file_path=r"D:\Project\machine_learning_project\config\schema.yaml"
+        # file_path=r"D:\Project\machine_learning_project\housing\artifact\data_ingestion\2022-06-27-19-13-17\ingested_data\train\housing.csv"
+
+        # df= DataTransformation.load_data(file_path=file_path,schema_file_path=schema_file_path)
+        # print(df.columns)
+        # print(df.dtypes)
+
     except Exception as e:
         logging.error(f"{e}")
         print(e)
 
+
+
 if __name__=="__main__":
     main()
-
